41ce4f2c9a686cab987ff9012116dab3f5d27b58
Richard-Thompson <richardthompson1990@hotmail.co.uk>
2017-07-18 11:00:34 +0100

    topNav completed

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import TopNav from './TopNav';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+      <TopNav />
+      
+      </div>
+    );
+  }
+}
+export default App;
